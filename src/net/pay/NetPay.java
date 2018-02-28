2159a08875e81067a50af0791bc31cb00dd7b588
尹小姐 <@10.5.142.141>
2018-02-28 11:07:29 -0500

    First commit. Net Pay.

---
+++ src/net/pay/NetPay.java
@@ -0,0 +1,18 @@
+/*
+ * Jiayin Xie, Jason
+ * Net Pay
+ * 
+ */
+package net.pay;
+
+public class NetPay {
+    public static void main(String[] args) {
+        int h=40;
+        int w=5;
+        int i=2;
+        double t=0.22;
+        double n=(h*w-i)-t*(h*w-i);
+        System.out.println("The net pay of the employee is "+n);
+    }
+    
+}
